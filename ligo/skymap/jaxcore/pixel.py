88889475ea7c7f711dae9337c4e70fed084e6a56
Jacob Davis <jldcraft@gmail.com>
2025-06-26 09:01:27 -0400

    Start pixel implementation

---
+++ ligo/skymap/jaxcore/pixel.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2025 Jacob Davis
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+from jax import jit, vmap, lax
+import jax
+import jax.numpy as jnp
+import time
+import timeit
+from functools import partial
+from interp import *
+from integrate import *
+from cosmology import *
+from moc import *
+from quadax import quadgk # type: ignore
+
+class bayestar_pixel:
+    def __init__(self, nest, order):
+        self.uniq = nest + (1 << 2 * (order + 1))
+
+
+
+# typedef struct {
+#     unsigned long long uniq;
+#     double value[3];
+# } bayestar_pixel;
